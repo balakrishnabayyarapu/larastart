af4d1a32d45fdb906570e18646729b2fe7ee1565
balakrishna <balakrishnabayyrapu@gmail.com>
2019-02-21 00:17:36 +0530

    initial commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>first vue and larave app</title>
+</head>
+<body>
+	<div id="app">
+		<example-component></example-component>
+	</div>
+
+	<script src="{{asset('./js/app.js')}}"></script>
+</body>
+</html>
